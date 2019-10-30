3776ad9d043a7fce91102ec691daf37869b2a82d
Christian Gustavo <christiangomes00@gmail.com>
2019-10-30 11:08:43 -0300

    take

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, filter } from 'rxjs/operators';
 
 import { Course } from '../model/course';
 import { createHttpObservable } from './util';
@@ -39,7 +39,8 @@ export class Store {
     selectCourseById(courseId: number) {
         return this.courses$
             .pipe(
-                map(courses => courses.find(course => course.id === courseId))
+                map(courses => courses.find(course => course.id === courseId)),
+                filter(course => !!course)
             );
     }
 
