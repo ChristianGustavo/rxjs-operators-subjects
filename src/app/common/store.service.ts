e015c86f6fb5aed26ebde92e87195a37b632664a
Christian Gustavo <christiangomes00@gmail.com>
2019-10-30 10:52:49 -0300

    Salvando curso com otimização UI para atualizar na view enquanto o backend reflete a alteração

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -4,6 +4,7 @@ import { map, tap } from 'rxjs/operators';
 
 import { Course } from '../model/course';
 import { createHttpObservable } from './util';
+import { fromPromise } from 'rxjs/internal-compatibility';
 
 @Injectable({
     providedIn: 'root'
@@ -41,6 +42,31 @@ export class Store {
                 map(courses => courses
                     .filter(course => course.category === category))
             );
+    }
+
+    saveCourse(courseId: number, changes): Observable<any> {
+
+        const courses = this.subject.getValue();
+
+        const courseIndex = courses.findIndex(course => course.id === courseId);
+
+        const newCourses = courses.slice(0);
+
+        newCourses[courseIndex] = {
+            ...courses[courseIndex],
+            ...changes
+        };
+
+        this.subject.next(newCourses);
+
+        return fromPromise(fetch(`/api/courses/${courseId}`, {
+            method: 'PUT',
+            body: JSON.stringify(changes),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }));
+
     }
 
 }
